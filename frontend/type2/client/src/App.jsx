10601fe77be439289d525ee927ca2111ab03960f
heartggs <jgggggj6789@gmail.com>
2025-01-06 20:14:06 +0900

    feat:네비게이션바 누락파일추개

--- frontend/type2/client/src/App.jsx
+++ frontend/type2/client/src/App.jsx
@@ -1,9 +1,5 @@
 import './App.css';
 import Navigation from "./common/Navigation/Navigation.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Pharmacy from "./page/Pharmacy.jsx";
-import Home from "./page/Home.jsx";
-import Favorites from "./page/Favorites.jsx";
 
 function App() {
     return (
