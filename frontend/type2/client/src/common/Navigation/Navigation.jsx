1e3a18cdd0001562a020c9ced3dfca7e6cf712e5
heartggs <jgggggj6789@gmail.com>
2025-01-10 18:10:11 +0900

    fix-지도페이지 모바일사이즈 변경 및 네브바 변경

--- frontend/type2/client/src/common/Navigation/Navigation.jsx
+++ frontend/type2/client/src/common/Navigation/Navigation.jsx
@@ -11,28 +11,30 @@ function Navigation() {
     const [active, setActive] = useState(1);
 
     const menuItmes = [
-        {id: 1, icon: <SearchIcon/>, path: "/search", label: "Search"},
+        {id: 3, icon: <SearchIcon/>, path: "/search", label: "Search"},
         {id: 2, icon: <SupplementsIcon/>, path: "/recommend", label: "Supplements"},
-        {id: 3, icon: <HomeIcon/>, path: "/", label: "Home"},
+        {id: 1, icon: <HomeIcon/>, path: "/", label: "Home"},
         {id: 4, icon: <FavoriteIcon/>, path: "/", label: "Favorite"},
         {id: 5, icon: <MyPageIcon/>, path: "/mypage", label: "MyPage"}
     ]
 
 
     return (
-        <nav className={styles.navBar}>
-            <ul>
-                {menuItmes.map((item) => (
-                    <li key={item.id}
-                        onClick={() => setActive(item.id)}
-                        className={active === item.id ? styles.active : ""}>
-                        <Link to={item.path}>
-                            {item.icon}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+        <div className={styles.navBar_wrapper}>
+            <nav className={styles.navBar}>
+                <ul>
+                    {menuItmes.map((item) => (
+                        <li key={item.id}
+                            onClick={() => setActive(item.id)}
+                            className={active === item.id ? styles.active : ""}>
+                            <Link to={item.path}>
+                                {item.icon}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
     );
 }
 
