f64a49959ffbe8c807cd6142934ab62eae75b91f
wonbeomsong <swb1913@gmail.com>
2025-01-11 22:47:57 +0900

    fix 게시판 오류 수정

--- frontend/type2/client/src/page/Home/Home.jsx
+++ frontend/type2/client/src/page/Home/Home.jsx
@@ -56,7 +56,7 @@ function Home(){
                     <Link to={`/post/${item.postId}`} className={styles.post} key={item.postId}>
                         <div className={styles.top_area}>
                             <p className={styles.title}>{item.title}</p>
-                            <p>{item.userId}</p>
+                            <p>{item.nickname}</p>
                         </div>
                         <div className={styles.content}>
                             <p>
