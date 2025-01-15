cada8847b2c3874df164b297f8cfdd35bfba1243
12ilya21 <12ilya21@naver.com>
2025-01-16 05:39:05 +0900

    fix: 반려동물, 영양제 api 수정

--- backend/server/src/main/java/org/example/server/service/NsService.java
+++ backend/server/src/main/java/org/example/server/service/NsService.java
@@ -7,7 +7,6 @@ import org.example.server.entities.Pet;
 import org.example.server.entities.User;
 import org.example.server.repository.NsRepository;
 import org.example.server.repository.PetRepository;
-import org.example.server.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
