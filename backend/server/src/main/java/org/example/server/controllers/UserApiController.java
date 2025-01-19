f720cb2768f59e2103e1b2eac778a74d8fb1c0b7
12ilya21 <12ilya21@naver.com>
2025-01-19 11:00:52 +0900

    fix: 로그아웃 api 오류 수정

--- backend/server/src/main/java/org/example/server/controllers/UserApiController.java
+++ backend/server/src/main/java/org/example/server/controllers/UserApiController.java
@@ -1,6 +1,5 @@
 package org.example.server.controllers;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
